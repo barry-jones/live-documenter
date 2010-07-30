3222e95bd6c27d8c686cdb1e92afdf9d714b3c95
Atirez <Atirez@34f4d40e-60c1-4fc8-9e04-d9087858da15>
2010-07-30 10:23:29 +0000

    Added comments

--- TheBoxSoftware.Reflection/Syntax/SyntaxToken.cs
+++ TheBoxSoftware.Reflection/Syntax/SyntaxToken.cs
@@ -5,17 +5,35 @@ using System.Text;
 
 namespace TheBoxSoftware.Reflection.Syntax {
 	/// <summary>
-	/// Base abstract class for all SyntaxToken types. A SyntaxToken is a
-	/// parsed syntax element that describes details about the token it
-	/// contains. This information can be used by display providers to
-	/// format the different types appropriately.
+	/// <para>A SyntaxToken is a string that represents an element of a syntax block. A 
+	/// SyntaxToken is a string and a <see cref="SyntaxTokens"/> type.</para>
 	/// </summary>
+	/// <remarks>
+	/// <para>A <see cref="IFormatter"/> will return a list of syntax tokens. Here is an
+	/// example of how to use SyntaxTokens:
+	/// </para>
+	/// <code>
+	/// List&lt;SyntaxToken&gt; tokens = new List&lt;SyntaxToken&gt;();
+	/// 
+	/// tokens.AddRange(this.GetVisibility(syntax));
+	/// tokens.Add(new SyntaxToken(" ", SyntaxTokens.Text));
+	/// tokens.Add(new SyntaxToken("const", SyntaxTokens.Keyword));
+	/// tokens.Add(new SyntaxToken(" ", SyntaxTokens.Text));
+	/// tokens.Add(this.GetType(syntax));
+	/// tokens.Add(new SyntaxToken(" ", SyntaxTokens.Text));
+	/// tokens.Add(new SyntaxToken(syntax.GetIdentifier(), SyntaxTokens.Text));
+	/// 
+	/// return tokens;
+	/// </code>
+	/// </remarks>
+	/// <seealso cref="SyntaxTokens"/>
 	public class SyntaxToken {
 		private SyntaxTokens tokenType;
 
 		/// <summary>
 		/// Initialises a new instance of the SyntaxToken class.
 		/// </summary>
+		/// <param name="content">The tokens content</param>
 		/// <param name="tokenType">The type of token.</param>
 		public SyntaxToken(string content, SyntaxTokens tokenType) {
 			this.Content = content;
