bf6fa09b94dd556664ae07e7e000b5a94efd5903
Atirez <Atirez@34f4d40e-60c1-4fc8-9e04-d9087858da15>
2010-08-19 11:30:15 +0000

    #117 put in extra checks to make sure we load the xml documentation file if it is available

--- TheBoxSoftware.DeveloperSuite.LiveDocumenter/DocumentationFileReader.cs
+++ TheBoxSoftware.DeveloperSuite.LiveDocumenter/DocumentationFileReader.cs
@@ -161,14 +161,29 @@ namespace TheBoxSoftware.DeveloperSuite.LiveDocumenter {
 		/// <summary>
 		/// Reads a project and returns all the reference project library files.
 		/// </summary>
-		private class ProjectFileReader : FileReader {
+		private abstract class ProjectFileReader : FileReader {
 			/// <summary>
 			/// Initialises a new instance of the ProjectFileReader class.
 			/// </summary>
 			/// <param name="fileName">The full path of the project file to read.</param>
 			public ProjectFileReader(string fileName) : base(fileName) { }
 
+			/// <summary>
+			/// Factory method for instantiating <see cref="ProjectFileReader" /> instances.
+			/// </summary>
+			/// <param name="filename">The name of the project file to read.</param>
+			/// <returns>An instance of a ProjectFileReader.</returns>
+			/// <remarks>
+			/// This method automatically determines which ProjectFileReader to instantiate
+			/// from the file provided.
+			/// </remarks>
+			/// <exception cref="ArgumentNullException">
+			/// Thrown when the provided <paramref name="filename"/> is null or empty.
+			/// </exception>
 			public static ProjectFileReader Create(string filename) {
+				if (string.IsNullOrEmpty(filename))
+					throw new ArgumentNullException("filename");
+
 				XmlDocument doc = new XmlDocument();
 				doc.Load(filename);
 
@@ -176,7 +191,7 @@ namespace TheBoxSoftware.DeveloperSuite.LiveDocumenter {
 				// but speed is of the essance! [#94]
 				if (doc.FirstChild.Name == "Project" || (doc.FirstChild.Name=="xml" && doc.FirstChild.NextSibling.Name == "Project")) {
 					TraceHelper.WriteLine("reading with 05 > ProjectFileReader");
-					return new ProjectFileReader(filename);
+					return new VS2005ProjectFileReader(filename);
 				}
 				else {
 					TraceHelper.WriteLine("reading with 03 < VS2003ProjectFileReader");
@@ -190,8 +205,122 @@ namespace TheBoxSoftware.DeveloperSuite.LiveDocumenter {
 			/// </summary>
 			/// <returns>An array of assembly files, although there will only ever be one from here.</returns>
 			public override List<DocumentedAssembly> Read() {
-				// TODO: Consider how we will know which is the selected output type
+				ProjectFileProperties properties = this.ParseProject();
 
+				if (!string.IsNullOrEmpty(properties.OutputPath)) {
+					string outputFile = string.Format(@"{0}\{1}{2}.{3}",
+									System.IO.Path.GetDirectoryName(this.FileName),
+									properties.OutputPath,
+									properties.LibraryName,
+									properties.GetOutputExtension());
+
+					string documentation = string.Empty;
+					if (!string.IsNullOrEmpty(properties.DocumentationFile)) {
+						if (System.IO.Path.IsPathRooted(properties.DocumentationFile)) {
+							documentation = properties.DocumentationFile;
+						}
+						else {
+							// We need to check if the file is relative to the project file or simply output
+							// in the output directory
+							documentation = System.IO.Path.GetFullPath(System.IO.Path.GetDirectoryName(this.FileName) + "\\" + properties.DocumentationFile);
+							if (!System.IO.File.Exists(documentation)) {
+								documentation = System.IO.Path.GetFullPath(System.IO.Path.GetDirectoryName(this.FileName) + "\\" + properties.OutputPath + "\\" + properties.DocumentationFile);
+
+								TraceHelper.WriteLineIf(!System.IO.File.Exists(documentation),
+									"Reading project properties and '{0}' documentation file was not found.",
+									documentation);
+							}
+						}
+					}
+
+					if (System.IO.File.Exists(outputFile)) {
+						return new List<DocumentedAssembly>() {
+							new DocumentedAssembly(outputFile, documentation)
+							};
+					}
+					else {
+						return new List<DocumentedAssembly>();
+					}
+				}
+				else {
+					throw new InvalidOperationException();
+				}
+			}
+
+			/// <summary>
+			/// Parses the contents of the <see cref="FileReader.FileName"/> and returns the
+			/// details to the caller.
+			/// </summary>
+			/// <returns>The relevant properties from the project files.</returns>
+			protected abstract ProjectFileProperties ParseProject();
+
+			/// <summary>
+			/// A data class that contains all the required information from the project
+			/// files.
+			/// </summary>
+			protected struct ProjectFileProperties {
+				/// <summary>
+				/// The type of output Library, etc.
+				/// </summary>
+				public string OutputType { get; set; }
+
+				/// <summary>
+				/// The name of the library
+				/// </summary>
+				public string LibraryName { get; set; }
+
+				/// <summary>
+				/// The path the library will be output to
+				/// </summary>
+				public string OutputPath { get; set; }
+
+				/// <summary>
+				/// The filename for the associated documentation file.
+				/// </summary>
+				public string DocumentationFile { get; set; }
+
+				/// <summary>
+				/// Calculates and returns the output extension depending on the <see cref="OutputType"/>.
+				/// </summary>
+				/// <returns>The correct file extension (only exe not .exe).</returns>
+				public string GetOutputExtension() {
+					string extension = string.Empty;
+					switch (this.OutputType.ToLower()) {
+						case "library":
+							extension = "dll";
+							break;
+						case "winexe":
+						case "exe":
+							extension = "exe";
+							break;
+						default:
+							break;
+					}
+					return extension;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Profile file reader that can be used to read the project files from Visual
+		/// Studio 2005, 2008 and 2010.
+		/// </summary>
+		/// <seealso cref="VS2003ProjectFileReader"/>
+		private class VS2005ProjectFileReader : ProjectFileReader {
+			/// <summary>
+			/// Initialises a new instance of the VS2003 project file reader.
+			/// </summary>
+			/// <param name="filename">The filename of the project</param>
+			public VS2005ProjectFileReader(string filename) :
+				base(filename) {
+			}
+
+			/// <summary>
+			/// Parses the contents of the <see cref="FileName"/> and returns the
+			/// details to the caller.
+			/// </summary>
+			/// <returns>The relevant properties from the project files.</returns>
+			protected override ProjectFileProperties ParseProject() {
 				XmlDocument projectFile = new XmlDocument();
 				projectFile.Load(this.FileName);
 
@@ -215,72 +344,44 @@ namespace TheBoxSoftware.DeveloperSuite.LiveDocumenter {
 				string outputPath = string.Empty;
 				string documentationPath = string.Empty;
 
-				switch(outputTypeNode.InnerText) {
-					case "Library":
-						outputExtension = "dll";
-						break;
-					case "WinExe":
-					case "Exe":
-						outputExtension = "exe";
-						break;
-					default:
-						break;
-				}
-
 				foreach (XmlNode currentNode in conditionalGroups) {
-					foreach(XmlAttribute attribute in currentNode.Attributes) {
+					foreach (XmlAttribute attribute in currentNode.Attributes) {
 						if (attribute.Name == "Condition" && (attribute.Value.IndexOf(Model.UserApplicationStore.Store.Preferences.BuildConfiguration.ToString(), StringComparison.InvariantCultureIgnoreCase) != -1)) {
 							XmlNode outPath = currentNode.SelectSingleNode("pr:OutputPath", namespaceManager);
 							XmlNode docPath = currentNode.SelectSingleNode("pr:DocumentationFile", namespaceManager);
 
-							if (outPath != null) outputPath = outPath.InnerText;
-							if (docPath != null) documentationPath = docPath.InnerText;
+							if (outPath != null)
+								outputPath = outPath.InnerText;
+							if (docPath != null)
+								documentationPath = docPath.InnerText;
 						}
 					}
 				}
 
-				if (!string.IsNullOrEmpty(outputPath)) {
-					string outputFile = string.Format(@"{0}\{1}{2}.{3}",
-							System.IO.Path.GetDirectoryName(this.FileName),
-							outputPath,
-							libraryName,
-							outputExtension);
-
-					string documentation = string.Empty;
-					if (System.IO.Path.IsPathRooted(documentationPath)) {
-						documentation = documentationPath;
-					}
-					else {
-						documentation = System.IO.Path.GetFullPath(System.IO.Path.GetDirectoryName(this.FileName) + "\\" + documentationPath);
-					}
-
-					if (System.IO.File.Exists(outputFile)) {
-						return new List<DocumentedAssembly>() {
-							new DocumentedAssembly(outputFile, documentation)
-							};
-					}
-					else {
-						return new List<DocumentedAssembly>();
-					}
-				}
-				else {
-					throw new InvalidOperationException();
-				}
+				ProjectFileProperties properties = new ProjectFileProperties();
+				properties.OutputType = outputTypeNode.InnerText;
+				properties.LibraryName = libraryName;
+				properties.OutputPath = outputPath;
+				properties.DocumentationFile = documentationPath;
+				return properties;
 			}
 		}
 
+		/// <summary>
+		/// Profile file reader that can be used to read the project files from Visual
+		/// Studio 2002 & 2003
+		/// </summary>
+		/// <seealso cref="VS2005ProjectFileReader"/>
 		private class VS2003ProjectFileReader : ProjectFileReader {
 			/// <summary>
 			/// Initialises a new instance of the VS2003 project file reader.
 			/// </summary>
 			/// <param name="filename">The filename of the project</param>
-			public VS2003ProjectFileReader(string filename) : 
+			public VS2003ProjectFileReader(string filename) :
 				base(filename) {
 			}
 
-			public override List<DocumentedAssembly> Read() {
-								// TODO: Consider how we will know which is the selected output type
-
+			protected override ProjectFileReader.ProjectFileProperties ParseProject() {
 				XmlDocument projectFile = new XmlDocument();
 				projectFile.Load(this.FileName);
 
@@ -294,8 +395,8 @@ namespace TheBoxSoftware.DeveloperSuite.LiveDocumenter {
 
 				// DocumentationFile
 				XmlNode settings = projectFile.SelectSingleNode(@"/VisualStudioProject/*/Build/Settings");
-				XmlNode debugNode = projectFile.SelectSingleNode(@"/VisualStudioProject/*/Build/Settings/Config[@Name='" + 
-					Model.UserApplicationStore.Store.Preferences.BuildConfiguration.ToString() + "']");
+				XmlNode debugNode = projectFile.SelectSingleNode(@"/VisualStudioProject/*/Build/Settings/Config[@Name='" +
+						Model.UserApplicationStore.Store.Preferences.BuildConfiguration.ToString() + "']");
 
 				string outputExtension = string.Empty;
 				string libraryName = settings.Attributes["AssemblyName"].Value;
@@ -306,45 +407,12 @@ namespace TheBoxSoftware.DeveloperSuite.LiveDocumenter {
 					documentationFile = debugNode.Attributes["DocumentationFile"].Value;
 				}
 
-				switch(settings.Attributes["OutputType"].Value) {
-					case "Library":
-						outputExtension = "dll";
-						break;
-					case "WinExe":
-					case "Exe":
-						outputExtension = "exe";
-						break;
-					default:
-						break;
-				}
-
-				if (!string.IsNullOrEmpty(outputPath)) {
-					string outputFile = string.Format(@"{0}\{1}{2}.{3}",
-							System.IO.Path.GetDirectoryName(this.FileName),
-							outputPath,
-							libraryName,
-							outputExtension);
-
-					string documentation = string.Empty;
-					if (System.IO.Path.IsPathRooted(documentationFile)) {
-						documentation = documentationFile;
-					}
-					else {
-						documentation = System.IO.Path.GetFullPath(System.IO.Path.GetDirectoryName(this.FileName) + "\\" + documentationFile);
-					}
-
-					if (System.IO.File.Exists(outputFile)) {
-						return new List<DocumentedAssembly>() {
-							new DocumentedAssembly(outputFile, documentation)
-							};
-					}
-					else {
-						return new List<DocumentedAssembly>();
-					}
-				}
-				else {
-					throw new InvalidOperationException();
-				}
+				ProjectFileProperties properties = new ProjectFileProperties();
+				properties.OutputType = settings.Attributes["OutputType"].Value;
+				properties.LibraryName = libraryName;
+				properties.OutputPath = outputPath;
+				properties.DocumentationFile = documentationFile;
+				return properties;
 			}
 		}
 
