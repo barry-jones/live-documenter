dd3c921e0a5142d01b4ed75f291cb45775518c1d
Atirez <Atirez@34f4d40e-60c1-4fc8-9e04-d9087858da15>
2010-08-25 09:47:32 +0000

    #120 added code to test 120 fix

---
+++ DocumentationTest/Issues/Issue120.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DocumentationTest.Issues {
+	/// <summary>
+	/// The application does not handle empty param tags.
+	/// <para></para>
+	/// </summary>
+	public class Issue120 {
+	}
+}
