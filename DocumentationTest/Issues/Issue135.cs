ea309c5e70d166aa5743fc98283dbddd89ac5b07
Atirez <Atirez@34f4d40e-60c1-4fc8-9e04-d9087858da15>
2011-03-02 13:10:45 +0000

    Formatting changes

--- DocumentationTest/Issues/Issue135.cs
+++ DocumentationTest/Issues/Issue135.cs
@@ -9,7 +9,6 @@ namespace DocumentationTest.Issues
 	/// <para><seealso cref="Issue9"/> That see also block will cause an exception.</para>
 	/// <para>Some text <seealso cref="Issue9"/> This will be invisible.</para>
 	/// </summary>
-	class Issue135
-	{
+	class Issue135 {
 	}
 }
