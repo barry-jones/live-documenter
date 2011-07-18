e8a794d574a254c86e8145d2977d9902e01fcc0d
Atirez <Atirez@34f4d40e-60c1-4fc8-9e04-d9087858da15>
2011-07-18 15:02:54 +0000

    Updated Live Documenter UI to use the new visibility code and updated image checking code

--- TheBoxSoftware.DeveloperSuite.LiveDocumenter/Model/ElementIconConstants.cs
+++ TheBoxSoftware.DeveloperSuite.LiveDocumenter/Model/ElementIconConstants.cs
@@ -12,8 +12,9 @@ namespace TheBoxSoftware.DeveloperSuite.LiveDocumenter.Model {
 	/// Class that handles the association of the VS icons to each of
 	/// the different elements (properties, methods etc).
 	/// </summary>
-    public sealed class ElementIconConstants {
-        private const string basePath = "Resources/ElementIcons/vsobject_{0}.png";
+	public sealed class ElementIconConstants
+	{
+		private const string basePath = "Resources/ElementIcons/vsobject_{0}.png";
 
 		/// <summary>
 		/// Obtains the path to an icon image for the specified object.
@@ -24,36 +25,38 @@ namespace TheBoxSoftware.DeveloperSuite.LiveDocumenter.Model {
 		/// <paramref name="o"/> does need to a <see cref="ReflectedMember"/>
 		/// at the moment. Other types will fail and return an empty string.
 		/// </remarks>
-		public static string GetIconPathFor(object o) {
+		public static string GetIconPathFor(object o)
+		{
 			string path = string.Empty;
 			if (o is ReflectedMember)
 				path = ElementIconConstants.GetIconPathFor((ReflectedMember)o);
-			else {
+			else
+			{
 			}
 			return path;
 		}
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="member"></param>
-        /// <returns></returns>
-        /// <remarks>
-        /// The order in which the visibility modifiers are checked is important as
-        /// they use the first 3 bits of the flag, and they can use the first bit to
-        /// represent private and the 3 and 1st bit to represent public.
-        /// </remarks>
-        private static string GetIconPathFor(ReflectedMember member) {
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="member"></param>
+		/// <returns></returns>
+		/// <remarks>
+		/// The order in which the visibility modifiers are checked is important as
+		/// they use the first 3 bits of the flag, and they can use the first bit to
+		/// represent private and the 3 and 1st bit to represent public.
+		/// </remarks>
+		private static string GetIconPathFor(ReflectedMember member)
+		{
 			string name = string.Empty;
 
 			if (member is AssemblyDef) {
 				name = "assembly";
 			}
-            else if(member is TypeDef) {
+			else if (member is TypeDef) {
 				name = "class";
 				TypeDef typeDef = (TypeDef)member;
 				if (typeDef != null) {
-
 					if (typeDef.IsInterface) {
 						name = "interface";
 					}
@@ -66,55 +69,12 @@ namespace TheBoxSoftware.DeveloperSuite.LiveDocumenter.Model {
 					else if (typeDef.IsEnumeration) {
 						name = "enum";
 					}
-
-                    if ((typeDef.Flags & TypeAttributes.NestedPublic) == TypeAttributes.NestedPublic) {
-                    }
-                    else if ((typeDef.Flags & TypeAttributes.NestedFamily) == TypeAttributes.NestedFamily) {
-                        name += "_protected";
-                    }
-                    else if ((typeDef.Flags & TypeAttributes.NestedAssembly) == TypeAttributes.NestedAssembly) {
-                        name += "_sealed";
-                    }
-                    else if ((typeDef.Flags & TypeAttributes.NestedPrivate) == TypeAttributes.NestedPrivate) {
-                        name += "_private";
-                    }
-                    else if ((typeDef.Flags & TypeAttributes.Public) == TypeAttributes.Public) {
-                    }
-                    else if ((typeDef.Flags & TypeAttributes.NotPublic) == TypeAttributes.NotPublic) {
-                        name += "_sealed";
-                    }
-                }
-			}
-			else if(member is EventDef) {
-				name = "event";
-				EventDef eventDef = (EventDef)member;
-
-				if (eventDef != null) {
-					MethodDef addMethod = eventDef.GetAddEventMethod();
-					MethodDef removeMethod = eventDef.GetRemoveEventMethod();
-					MethodAttributes addVisibility = addMethod != null
-						? addMethod.Attributes & MethodAttributes.MemberAccessMask
-						: 0;
-					MethodAttributes removeVisibility = removeMethod != null
-						? removeMethod.Attributes & MethodAttributes.MemberAccessMask
-						: 0;
-					MethodAttributes visibility = (((int)addVisibility) > ((int)removeVisibility))
-						? addVisibility
-						: removeVisibility;
-					if ((visibility & MethodAttributes.Public) == MethodAttributes.Public) {
-					}
-					else if ((visibility & MethodAttributes.Family) == MethodAttributes.Family) {
-						name += "_protected";
-					}
-					else if ((visibility & MethodAttributes.Assem) == MethodAttributes.Assem) {
-						name += "_sealed";
-					}
-					else if ((visibility & MethodAttributes.Private) == MethodAttributes.Private) {
-						name += "_private";
-					}
 				}
 			}
-			else if(member is FieldDef) {
+			else if (member is EventDef) {
+				name = "event";
+			}
+			else if (member is FieldDef) {
 				name = "field";
 				FieldDef fieldDef = (FieldDef)member;
 				if (fieldDef.IsConstant) {
@@ -123,66 +83,37 @@ namespace TheBoxSoftware.DeveloperSuite.LiveDocumenter.Model {
 				else if (fieldDef.IsOperator) {
 					name = "operator";
 				}
-
-                if ((fieldDef.Flags & FieldAttributes.Public) == FieldAttributes.Public) {
-                }
-                else if ((fieldDef.Flags & FieldAttributes.Family) == FieldAttributes.Family) {
-                    name += "_protected";
-                }
-                else if ((fieldDef.Flags & FieldAttributes.Assembly) == FieldAttributes.Assembly) {
-                    name += "_sealed";
-                }
-                else if ((fieldDef.Flags & FieldAttributes.Private) == FieldAttributes.Private) {
-                    name += "_private";
-                }
 			}
-			else if(member is MethodDef) {
+			else if (member is MethodDef) {
 				name = "method";
-				MethodDef methodDef = (MethodDef)member;
+			}
+			else if (member is PropertyDef) {
+				name = "properties";
+			}
 
-                if ((methodDef.Attributes & MethodAttributes.Public) == MethodAttributes.Public) {
-                    // stay at method
-                }
-                else if ((methodDef.Attributes & MethodAttributes.Family) == MethodAttributes.Family) {
-                    name = "method_protected";
-                }
-                else if ((methodDef.Attributes & MethodAttributes.Assem) == MethodAttributes.Assem) {
-                    name = "method_sealed";
-                }
-				else if ((methodDef.Attributes & MethodAttributes.Private) == MethodAttributes.Private) {
-					name = "method_private";
+			// add the visibility modifier
+			if (member is TypeDef || member is EventDef || member is FieldDef || member is MethodDef || member is PropertyDef) {
+				switch (member.MemberAccess) {
+					case Visibility.Protected:
+						name += "_protected";
+						break;
+					case Visibility.Internal:
+						name += "_sealed";
+						break;
+					case Visibility.InternalProtected:
+						name += "_friend";
+						break;
+					case Visibility.Private:
+						name += "_private";
+						break;
+					case Visibility.Public:
+						break;
 				}
 			}
-            else if (member is PropertyDef) {
-                name = "properties";
-                PropertyDef propertyDef = (PropertyDef)member;
-                
-                if (propertyDef != null) {
-                    MethodAttributes getterVisibility = propertyDef.GetMethod != null 
-                        ? propertyDef.GetMethod.Attributes & MethodAttributes.MemberAccessMask
-                        : 0;
-                    MethodAttributes setterVisibility = propertyDef.SetMethod != null
-                        ? propertyDef.SetMethod.Attributes & MethodAttributes.MemberAccessMask
-                        : 0;
-                    MethodAttributes visibility = (((int)getterVisibility) > ((int)setterVisibility))
-                        ? getterVisibility
-                        : setterVisibility;
-                    if ((visibility & MethodAttributes.Public) == MethodAttributes.Public) {
-                    }
-                    else if ((visibility & MethodAttributes.Family) == MethodAttributes.Family) {
-                        name += "_protected";
-                    }
-                    else if ((visibility & MethodAttributes.Assem) == MethodAttributes.Assem) {
-                        name += "_sealed";
-                    }
-                    else if ((visibility & MethodAttributes.Private) == MethodAttributes.Private) {
-                        name += "_private";
-                    }
-                }
-            }
+
 			// TODO: Handle namespaces and assemblies
 
-            return string.IsNullOrEmpty(name) ? string.Empty : string.Format(basePath, name);
-        }
-    }
+			return string.IsNullOrEmpty(name) ? string.Empty : string.Format(basePath, name);
+		}
+	}
 }
