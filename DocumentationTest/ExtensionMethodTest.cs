56c72625c83d940ad9f357263bc2f547886a315d
Atirez <Atirez@34f4d40e-60c1-4fc8-9e04-d9087858da15>
2011-08-07 10:15:22 +0000

    Added test extension method

---
+++ DocumentationTest/ExtensionMethodTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DocumentationTest {
+	public static class ExtensionMethodTest {
+		public static int ExtensionMethod(this AllOutputTypesClass c) {
+			return 1;
+		}
+	}
+}
