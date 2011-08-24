1eac29613a6c65c0804042882772dd2edc2f22dd
Atirez <Atirez@34f4d40e-60c1-4fc8-9e04-d9087858da15>
2011-08-24 18:34:53 +0000

    added tests for xml comments

---
+++ DocumentationTest/CommentTests/SeeElement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DocumentationTest.CommentTests {
+	/// <summary>
+	///	This class tests various uses and basterdisations of the see element.
+	/// </summary>
+	/// <remarks>
+	/// The <see cref="SeeElement"/> can be used to link to other elements.
+	/// </remarks>
+	class SeeElement {
+		/// <summary>
+		/// This tests the casing of the element and its attributes.
+		/// </summary>
+		/// <remarks>
+		/// We can <see cref="InvalidCasing"/>! <SEE cref="InvalidCasing"/> and <see CREF="InvalidCasing"/>.
+		/// </remarks>
+		void InvalidCasing() { }
+	}
+}
