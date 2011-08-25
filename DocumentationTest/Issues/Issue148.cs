b482da5182051f8991c9ab841cedd6ef15ff3403
Atirez <Atirez@34f4d40e-60c1-4fc8-9e04-d9087858da15>
2011-08-25 09:54:56 +0000

    #148 added code to test the problem

---
+++ DocumentationTest/Issues/Issue148.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security;
+using System.Runtime.InteropServices;
+
+namespace DocumentationTest.Issues
+{
+	class Issue148
+	{
+		[return: MarshalAs(UnmanagedType.Bool)]
+		[SuppressUnmanagedCodeSecurity]
+		bool GetKeepOriginalFormat();
+	}
+}
