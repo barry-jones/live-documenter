0644341102d6a9982f6ed8ad9ab6385ad56412d1
Atirez <Atirez@34f4d40e-60c1-4fc8-9e04-d9087858da15>
2011-09-10 14:33:01 +0000

    simplified version updates

--- TheBoxSoftware.DeveloperSuite.LiveDocumenter.Exporter/Properties/AssemblyInfo.cs
+++ TheBoxSoftware.DeveloperSuite.LiveDocumenter.Exporter/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e2fa5b3d-009d-4748-a6c3-96d9a5db069c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.*")]
-[assembly: AssemblyFileVersion("1.2.0.1")]
