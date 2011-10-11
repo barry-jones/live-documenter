a72554406f7ab47618aed320013527a3d28256f5
Atirez <Atirez@34f4d40e-60c1-4fc8-9e04-d9087858da15>
2011-10-11 12:53:07 +0000

    Added more tests

---
+++ DocumentationTest/TestException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DocumentationTest {
+	/// <summary>
+	/// A test exception.
+	/// </summary>
+	public class TestException : Exception {
+	}
+}
