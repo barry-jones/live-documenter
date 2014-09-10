73f92c59058388123dfee38239d333feed1a892a
Barry Jones <github@barryjoens.me.uk>
2014-09-10 18:32:09 +0100

    Added a simple license encryption/decreption code base
    Added a simple test to check generation of license files.

---
+++ 1. Tests/Test.Licensing/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TheBoxSoftware.Licensing;
+
+namespace Test.Licensing
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.TestGenerateLicenseFile();
+            p.TestReadLicenseFile();
+        }
+
+        private void TestGenerateLicenseFile()
+        {
+            License l = new License();
+            l.Name = "Barry Jones";
+            l.Email = "licensed@barryjones.me.uk";
+            l.Start = DateTime.Now;
+
+            l.Components.Add("ld-desktop", 1);
+            l.Components.Add("ld-server", 1);
+            l.Components.Add("ld-api", 1);
+
+            byte[] file = l.Encrypt();
+            System.IO.File.WriteAllBytes(@"test.lic", file);
+        }
+
+        private void TestReadLicenseFile()
+        {
+            License l = License.Decrypt(@"test.lic");
+
+            Console.WriteLine(l.Name);
+            Console.WriteLine(l.Email);
+            Console.WriteLine(l.Start.ToShortDateString());
+            foreach (KeyValuePair<string, int> current in l.Components)
+            {
+                Console.WriteLine(string.Format("component: {0} version: {1}", current.Key, current.Value));
+            }
+        }
+    }
+}
--- TheBoxSoftware/Licensing/License.cs
+++ TheBoxSoftware/Licensing/License.cs
@@ -1,46 +1,162 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Security.Cryptography;
 using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
 
-namespace TheBoxSoftware.Licencing
+namespace TheBoxSoftware.Licensing
 {
+    /// <summary>
+    /// Represents the details of a License which determines what functionliaty (if any) is available
+    /// at run time.
+    /// </summary>
     [Serializable]
     public sealed class License
     {
-        private DateTime purchaseDate;
-        private DateTime licencedUntil;
-        private List<string> components;
+        private const string phrase = "49b76s9954";
+        private DateTime start;
+        private DateTime end;
+        private string name;
+        private string email;
+        private Dictionary<string,int> components = new Dictionary<string,int>();
 
-        public static License Decrypt()
+        // This constant string is used as a "salt" value for the PasswordDeriveBytes function calls.
+        // This size of the IV (in bytes) must = (keysize / 8).  Default keysize is 256, so the IV must be
+        // 32 bytes long.  Using a 16 character string here gives us 32 bytes when converted to a byte array.
+        private static readonly byte[] initVectorBytes = Encoding.ASCII.GetBytes("tu89geji340t89u2");
+
+        // This constant is used to determine the keysize of the encryption algorithm.
+        private const int keysize = 256;
+
+        /// <summary>
+        /// Encrypts the current instance to a byte array.
+        /// </summary>
+        /// <returns>A byte array of encrypted data.</returns>
+        public byte[] Encrypt()
         {
-            string pubKey = "";
+            byte[] plainTextBytes = null;
 
+            // serialize this class
+            BinaryFormatter formatter = new BinaryFormatter();
+            using(MemoryStream s = new MemoryStream())
+            {
+                formatter.Serialize(s, this);
+                s.Seek(0, SeekOrigin.Begin);
+                plainTextBytes = new byte[s.Length];
+                s.Read(plainTextBytes, 0, (int)s.Length);
+            }
 
-            return new License();
+            
+            PasswordDeriveBytes password = new PasswordDeriveBytes(phrase, null);
+
+            byte[] keyBytes = password.GetBytes(keysize / 8);
+            using (RijndaelManaged symmetricKey = new RijndaelManaged())
+            {
+                symmetricKey.Mode = CipherMode.CBC;
+                using (ICryptoTransform encryptor = symmetricKey.CreateEncryptor(keyBytes, initVectorBytes))
+                {
+                    using (MemoryStream memoryStream = new MemoryStream())
+                    {
+                        using (CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                        {
+                            cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                            cryptoStream.FlushFinalBlock();
+                            byte[] cipherTextBytes = memoryStream.ToArray();
+                            return cipherTextBytes;
+                        }
+                    }
+                }
+            }
         }
 
-        public override string ToString()
+        /// <summary>
+        /// Decrypts the specified file to an instance of License.
+        /// </summary>
+        /// <param name="file">The file to decrypt/</param>
+        /// <returns>A license containing licensing information or null if license was not found or is invalid.</returns>
+        public static License Decrypt(string file)
         {
-            return base.ToString();
+            if (!File.Exists(file))
+            {
+                return null;
+            }
+
+            byte[] plainTextBytes;
+            byte[] cipherTextBytes = File.ReadAllBytes(file);
+            PasswordDeriveBytes password = new PasswordDeriveBytes(phrase, null);
+
+            byte[] keyBytes = password.GetBytes(keysize / 8);
+            using(RijndaelManaged symmetricKey = new RijndaelManaged())
+            {
+                symmetricKey.Mode = CipherMode.CBC;
+                using(ICryptoTransform decryptor = symmetricKey.CreateDecryptor(keyBytes, initVectorBytes))
+                {
+                    using(MemoryStream memoryStream = new MemoryStream(cipherTextBytes))
+                    {
+                        using(CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                        {
+                            plainTextBytes = new byte[cipherTextBytes.Length];
+                            int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+                        }
+                    }
+                }
+            }
+
+            // deserialize
+            License lic = null;
+            BinaryFormatter formatter = new BinaryFormatter();
+            using(MemoryStream s = new MemoryStream(plainTextBytes))
+            {
+                lic = (License)formatter.Deserialize(s);
+            }
+
+            return lic;
         }
 
-        public List<string> Components
+        /// <summary>
+        /// Gets or sets the display name of the account holder.
+        /// </summary>
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        /// <summary>
+        /// Gest or sets the account id for the account holder.
+        /// </summary>
+        public string Email
+        {
+            get { return this.email; }
+            set { this.email = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the components that are licensed.
+        /// </summary>
+        public Dictionary<string,int> Components
         {
             get { return this.components; }
             set { this.components = value; }
         }
 
-        public DateTime LicencedUntil
+        /// <summary>
+        /// Gets or sets a date that indicates when the license will expire - can be null.
+        /// </summary>
+        public DateTime End
         {
-            get { return this.licencedUntil; }
-            set { this.licencedUntil = value; }
+            get { return this.end; }
+            set { this.end = value; }
         }
 
-        public DateTime PurchaseDate
+        /// <summary>
+        /// Gets or sets the start date for the license.
+        /// </summary>
+        public DateTime Start
         {
-            get { return this.purchaseDate; }
-            set { this.purchaseDate = value; }
+            get { return this.start; }
+            set { this.start = value; }
         }
     }
 }
