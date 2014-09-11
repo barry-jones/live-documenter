2bd341ea3664d8c212b01dfd8864c00836e1f464
Barry Jones <github@barryjoens.me.uk>
2014-09-11 02:57:49 +0100

    Fixes #6 - no longer try and add exension methods to types that can not be found.
    Added what may have been unessary code for testing similar things between different version of .net.

---
+++ 1. Tests/Framework.Tests/v2.0/v2/ExtensionMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsageAttribute(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Method)]
+    public class ExtensionAttribute : Attribute
+    {
+    }
+}
+
+namespace v2._0.v2
+{
+
+    public static class ExtensionMethods
+    {
+        /// <summary>
+        /// A .net 2 runtime extension method
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string ExentsionMethod(this string str)
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ 1. Tests/Framework.Tests/v4.0/v4/DynamicKeyword.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace v4._0.v4
+{
+    public class DynamicKeyword
+    {
+        /// <summary>
+        /// A silly dynamic method
+        /// </summary>
+        /// <param name="aProperty"></param>
+        /// <returns></returns>
+        public dynamic Method(dynamic aProperty)
+        {
+            return null;
+        }
+    }
+}
