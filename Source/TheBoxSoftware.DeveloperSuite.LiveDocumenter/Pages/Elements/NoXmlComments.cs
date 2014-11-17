1c539cc5cbe1cb14c1bd082eacbd7178695fb725
Barry Jones <github@barryjones.me.uk>
2014-11-17 10:29:56 +0000

    resolves #27. Added a link to documentation on teh site detailing how to get the XML comments to be output.

--- Source/TheBoxSoftware.DeveloperSuite.LiveDocumenter/Pages/Elements/NoXmlComments.cs
+++ Source/TheBoxSoftware.DeveloperSuite.LiveDocumenter/Pages/Elements/NoXmlComments.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Windows;
 using System.Windows.Documents;
 using TheBoxSoftware.Reflection;
+using System.Diagnostics;
+using System.Windows.Navigation;
 
 namespace TheBoxSoftware.DeveloperSuite.LiveDocumenter.Pages.Elements {
 	internal class NoXmlComments : Paragraph {
@@ -13,8 +15,19 @@ namespace TheBoxSoftware.DeveloperSuite.LiveDocumenter.Pages.Elements {
 			this.Style = (Style)this.FindResource("NoComments");
 
 			this.Inlines.Add(new Run(
-				string.Format("No XML comments file found for declaring assembly '{0}'.", System.IO.Path.GetFileName(entry.Assembly.File.FileName))
+				string.Format("No XML comments file found for declaring assembly '{0}'. ", System.IO.Path.GetFileName(entry.Assembly.File.FileName))
 				));
+
+            Hyperlink info = new Hyperlink(new Run("More information."));
+            info.NavigateUri = new Uri("http://documentation.livedocumenter.com/no-xml-comments");
+            info.RequestNavigate += info_RequestNavigate;
+            this.Inlines.Add(info);
 		}
+
+        void info_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
 	}
 }
