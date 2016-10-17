4bf6b18b75f01336a3d8994b5f08d73a6dc101bf
barry <atirez@bitbucket.org>
2016-10-17 18:57:58 +0100

    formatting changes to files and added base test class for PeCoffFile

---
+++ Source/TheBoxSoftware.Reflections.Tests/Core/Unit/PeCoffFileTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using TheBoxSoftware.Reflection.Core;
+
+namespace TheBoxSoftware.Reflections.Tests.Core.Unit
+{
+    [TestFixture]
+    public class PeCoffFileTests
+    {
+        [Test]
+        public void Create()
+        {
+            PeCoffFile coffFile = new PeCoffFile(string.Empty);
+        }
+    }
+}
