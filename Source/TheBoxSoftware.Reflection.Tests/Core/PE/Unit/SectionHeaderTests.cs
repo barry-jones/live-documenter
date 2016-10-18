258ec9610ff5025455189e2d5b06f6d8171a1564
Barry Jones <atirez@bitbucket.org>
2016-10-18 21:06:16 +0100

    added tests for the sectionheaders class

---
+++ Source/TheBoxSoftware.Reflection.Tests/Core/PE/Unit/SectionHeaderTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TheBoxSoftware.Reflection.Core;
+using TheBoxSoftware.Reflection.Core.PE;
+
+namespace TheBoxSoftware.Reflection.Tests.Core.PE.Unit
+{
+    [TestFixture]
+    public class SectionHeaderTests
+    {
+        [Test]
+        public void SectionHeader_WhenNameIsTerminated_NameDoesntContainTerminationCharacter()
+        {
+            byte[] contents = {
+                (byte)'t', (byte)'e', (byte)'s', (byte)'t', (byte)'\0', 0, 0, // first 8 are name
+                // just padd out the rest with zeros, they are not requried for the test
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+            };
+            Offset offset = 0;
+
+            SectionHeader sectionHeader = new SectionHeader(contents, offset);
+
+            Assert.AreEqual(4, sectionHeader.Name.Length);
+        }
+    }
+}
