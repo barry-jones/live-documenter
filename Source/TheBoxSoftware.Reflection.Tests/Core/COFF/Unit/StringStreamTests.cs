804c5ec6e7eb23e28a58206210df2b6ecc31446f
Barry Jones <atirez@bitbucket.org>
2016-10-19 13:43:35 +0100

    added more tests for the stringnstream class

--- Source/TheBoxSoftware.Reflection.Tests/Core/COFF/Unit/StringStreamTests.cs
+++ Source/TheBoxSoftware.Reflection.Tests/Core/COFF/Unit/StringStreamTests.cs
@@ -37,6 +37,18 @@ namespace TheBoxSoftware.Reflection.Tests.Core.COFF.Unit
         }
 
         [Test]
+        public void StringStream_GetAll_WhenNoStrings_ReturnsCorrectNumberOfStrings()
+        {
+            const int EXPECTED = 1; // includes only the first valid empty string
+            byte[] contents = { 0 };
+            StringStream stream = new StringStream(contents, 0, 1);
+
+            Dictionary<int, string> allStrings = stream.GetAllStrings();
+
+            Assert.AreEqual(EXPECTED, allStrings.Count);
+        }
+
+        [Test]
         public void StringStream_GetString_WhenValidStringRequested_ReturnsCorrectString()
         {
             const int POSITION_OF_STRING = 260;
@@ -47,6 +59,17 @@ namespace TheBoxSoftware.Reflection.Tests.Core.COFF.Unit
             string returned = stream.GetString(POSITION_OF_STRING);
 
             Assert.AreEqual(REQUESTED, returned);
+        }
+
+        [Test]
+        public void StringStream_GetString_WhenInvalidStringRequested_ReturnsEmptyString()
+        {
+            byte[] contents = { 0 };
+            StringStream stream = new StringStream(contents, 0, 1);
+
+            string returned = stream.GetString(260);
+
+            Assert.AreEqual(string.Empty, returned);
         }
 
         private StringStream CreateStringStream()
