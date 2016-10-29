c8caf6b33600ffa55c3305f2b8091d714a4313f7
barry <atirez@bitbucket.org>
2016-10-29 13:58:13 +0100

    missed checkin on previous commit.

---
+++ Source/TheBoxSoftware.Reflection.Tests/Unit/Signitures/CallingConventionSignitureTokenTests.cs
@@ -0,0 +1,32 @@
+﻿
+namespace TheBoxSoftware.Reflection.Tests.Unit.Signitures
+{
+    using NUnit.Framework;
+    using Reflection.Signitures;
+
+    [TestFixture]
+    public class CallingConventionSignitureTokenTests
+    {
+        [Test]
+        public void CallingConventionToken_Create_WhenProvidedACorrectValue_ShouldReturnSameValue()
+        {
+            byte[] content = new byte[] { (byte)CallingConventions.Default };
+
+            CallingConventionSignitureToken token = new CallingConventionSignitureToken(content, 0);
+
+            Assert.AreEqual(CallingConventions.Default, token.Convention);
+        }
+
+        [Test]
+        public void CallingConventionToken_ToString_WhenProvidedDefaultConvention_OutputsCorrectly()
+        {
+            byte[] content = new byte[] { (byte)CallingConventions.Default };
+
+            CallingConventionSignitureToken token = new CallingConventionSignitureToken(content, 0);
+
+            string result = token.ToString();
+
+            Assert.AreEqual("[CallingConvention: Default]", result);
+        }
+    }
+}
