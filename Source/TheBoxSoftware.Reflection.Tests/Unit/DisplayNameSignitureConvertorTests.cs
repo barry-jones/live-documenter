7c882a3e864e9bf821220ecd27e3c4b1acc25024
barry <atirez@bitbucket.org>
2016-11-05 10:17:35 +0000

    added test file for displaynamesignitureconvertor class.

---
+++ Source/TheBoxSoftware.Reflection.Tests/Unit/DisplayNameSignitureConvertorTests.cs
@@ -0,0 +1,17 @@
+﻿
+namespace TheBoxSoftware.Reflection.Tests.Unit
+{
+    using NUnit.Framework;
+    using Reflection.Signitures;
+
+    [TestFixture]
+    public class DisplayNameSignitureConvertorTests
+    {
+        [Test]
+        public void Create()
+        {
+            TypeDef type = new TypeDef();
+            var convertor = new DisplayNameSignitureConvertor(type, false);
+        }
+    }
+}
