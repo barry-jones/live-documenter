0ab2026dd21667c12169fc6a5579383840632cf0
barry <atirez@bitbucket.org>
2016-11-06 11:00:41 +0000

    added tests for AssemblyOS metadata and added extra check in Assembly metadata to make sure the offset is moved on enough fields.

--- Source/TheBoxSoftware.Reflection.Tests/Unit/Core/COFF/AssemblyMetadataTableRowTests.cs
+++ Source/TheBoxSoftware.Reflection.Tests/Unit/Core/COFF/AssemblyMetadataTableRowTests.cs
@@ -2,6 +2,7 @@
 namespace TheBoxSoftware.Reflection.Tests.Unit.Core.COFF
 {
     using NUnit.Framework;
+    using Reflection.Core;
     using Reflection.Core.COFF;
 
     [TestFixture]
@@ -12,6 +13,7 @@ namespace TheBoxSoftware.Reflection.Tests.Unit.Core.COFF
         {
             byte sizeOfStringIndexes = 2;
             byte sizeOfBlobIndexes = 2;
+            Offset offset = 0;
             byte[] contents = {
                 0x04, 0x80, 0x00, 0x00,
                 0x01, 0x00,
@@ -24,7 +26,7 @@ namespace TheBoxSoftware.Reflection.Tests.Unit.Core.COFF
                 0x00, 0x00
             };
 
-            AssemblyMetadataTableRow row = new AssemblyMetadataTableRow(contents, 0, sizeOfBlobIndexes, sizeOfStringIndexes);
+            AssemblyMetadataTableRow row = new AssemblyMetadataTableRow(contents, offset, sizeOfBlobIndexes, sizeOfStringIndexes);
 
             Assert.AreEqual(AssemblyHashAlgorithms.SHA1, row.HashAlgId);
             Assert.AreEqual("1.0.0.0", row.GetVersion().ToString());
@@ -32,6 +34,8 @@ namespace TheBoxSoftware.Reflection.Tests.Unit.Core.COFF
             Assert.AreEqual(0, row.PublicKey);
             Assert.AreEqual(0x1D9A, row.Name.Value);
             Assert.AreEqual(0x0000, row.Culture.Value);
+            // read 22 bytes index 21, and ready to read 23 bytes index 22
+            Assert.AreEqual(22, offset.Current);
         }
     }
 }
---
+++ Source/TheBoxSoftware.Reflection.Tests/Unit/Core/COFF/AssemblyOSMetadataTableRowTests.cs
@@ -0,0 +1,26 @@
+﻿
+namespace TheBoxSoftware.Reflection.Tests.Unit.Core.COFF
+{
+    using NUnit.Framework;
+    using Reflection.Core;
+    using Reflection.Core.COFF;
+
+    [TestFixture]
+    public class AssemblyOSMetadataTableRowTests
+    {
+        [Test]
+        public void AssemblyOS_WhenConstructed_AllFieldsShouldAlwaysBeZero()
+        {
+            byte[] contents = { 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0 };
+            Offset offset = 0;
+
+            AssemblyOSMetadataTableRow row = new AssemblyOSMetadataTableRow(contents, offset);
+
+            Assert.AreEqual(0, row.OSPlatformID);
+            Assert.AreEqual(0, row.OSMajorVersion);
+            Assert.AreEqual(0, row.OSMinorVersion);
+            // offset is moved on 12 bytes to index 11 and ready to read next byte index 12
+            Assert.AreEqual(12, offset.Current); 
+        }
+    }
+}
