64fd656924dbc4aec9850aa17e2a74a1a677e407
barry <atirez@bitbucket.org>
2016-11-07 09:28:11 +0000

    added eventattributes rather than just using ushort. changed to use explicit backing fields.

---
+++ Source/TheBoxSoftware.Reflection/Core/COFF/EventAttributes.cs
@@ -0,0 +1,13 @@
+﻿
+namespace TheBoxSoftware.Reflection.Core.COFF
+{
+    using System;
+
+    [Flags]
+    public enum EventAttributes : short
+    {
+        None = 0x0000,
+        SpecialName = 0x0200,
+        RTSpecialName = 0x0400
+    }
+}
--- Source/TheBoxSoftware.Reflection/Core/COFF/EventMetadataTableRow.cs
+++ Source/TheBoxSoftware.Reflection/Core/COFF/EventMetadataTableRow.cs
@@ -5,6 +5,10 @@ namespace TheBoxSoftware.Reflection.Core.COFF
 
     public class EventMetadataTableRow : MetadataRow
     {
+        private CodedIndex _eventTypeIndex;
+        private StringIndex _nameIndex;
+        private EventAttributes _eventFlags;
+
         /// <summary>
         /// Initialises a new instance of the EventMetadataTableRow class
         /// </summary>
@@ -13,25 +17,38 @@ namespace TheBoxSoftware.Reflection.Core.COFF
         public EventMetadataTableRow(MetadataStream stream, byte[] contents, Offset offset)
         {
             this.FileOffset = offset;
-            this.EventFlags = BitConverter.ToUInt16(contents, offset.Shift(2));
-            this.Name = new StringIndex(stream, offset);
-            this.EventType = new CodedIndex(stream, offset, CodedIndexes.TypeDefOrRef);
+
+            _eventFlags = (EventAttributes)BitConverter.ToUInt16(contents, offset.Shift(2));
+            _nameIndex = new StringIndex(stream, offset);
+            _eventTypeIndex = new CodedIndex(stream, offset, CodedIndexes.TypeDefOrRef);
         }
 
         /// <summary>
         /// EventAttributes mask
         /// </summary>
-        public UInt16 EventFlags { get; set; }
+        public EventAttributes EventFlags
+        {
+            get { return _eventFlags; }
+            set { _eventFlags = value; }
+        }
 
         /// <summary>
         /// An index in the string heap
         /// </summary>
-        public StringIndex Name { get; set; }
+        public StringIndex Name
+        {
+            get { return _nameIndex; }
+            set { _nameIndex = value; }
+        }
 
         /// <summary>
         /// An index into a TypeDef, TypeRef, or TypeSpec table, more precisely
         /// a TypeDefOrRef encoded index
         /// </summary>
-        public CodedIndex EventType { get; set; }
+        public CodedIndex EventType
+        {
+            get { return _eventTypeIndex; }
+            set { _eventTypeIndex = value; }
+        }
     }
 }
