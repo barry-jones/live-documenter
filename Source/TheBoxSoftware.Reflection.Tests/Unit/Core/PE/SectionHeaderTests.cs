fc7a1a5fcd989c5bfa060c44443b6c8bf9f951c5
barry <atirez@bitbucket.org>
2016-11-08 10:43:37 +0000

    added more tests for the section header.

--- Source/TheBoxSoftware.Reflection.Tests/Unit/Core/PE/SectionHeaderTests.cs
+++ Source/TheBoxSoftware.Reflection.Tests/Unit/Core/PE/SectionHeaderTests.cs
@@ -23,5 +23,33 @@ namespace TheBoxSoftware.Reflection.Tests.Unit.Core.PE
 
             Assert.AreEqual(4, sectionHeader.Name.Length);
         }
+
+        [Test]
+        public void SectionHeader_WhenNameUsesAll8BytesWithNoTerminator_NameIs8CharactersLong()
+        {
+            byte[] contents = new byte[] {
+                (byte)'a',(byte)'b',(byte)'c',(byte)'d',(byte)'e',(byte)'f',(byte)'g',(byte)'h', 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+            };
+            Offset offset = 0;
+
+            SectionHeader sectionHeader = new SectionHeader(contents, offset);
+
+            Assert.AreEqual(8, sectionHeader.Name.Length);
+            Assert.AreEqual("abcdefgh", sectionHeader.Name);
+        }
+
+        [Test]
+        public void SectionHeader_WhenCreated_OffsetIsMovedOn()
+        {
+            byte[] contents = new byte[50];
+            Offset offset = 0;
+
+            SectionHeader header = new SectionHeader(contents, offset);
+
+            Assert.AreEqual(40, offset.Current);
+        }
     }
 }
