d330c8d0acb95b7bcae670aeafd587f9ec4a47f1
barry <atirez@bitbucket.org>
2016-11-08 12:48:04 +0000

    changed to use explicit backing fields.

--- Source/TheBoxSoftware.Reflection/Core/COFF/Cor20Header.cs
+++ Source/TheBoxSoftware.Reflection/Core/COFF/Cor20Header.cs
@@ -7,22 +7,35 @@ namespace TheBoxSoftware.Reflection.Core.COFF
     // REFERS: IMAGE_COR20_HEADER
     public class Cor20Header
     {
+        private ulong _cb;
+        private ushort _majorRuntimeVersion;
+        private ushort _minorRuntimeVersion;
+        private DataDirectory _metadata;
+        private Cor20Flags _flags;
+        private ulong _entryPointToken;
+        private DataDirectory _resources;
+        private DataDirectory _strongNameSigniture;
+        private DataDirectory _codeManagerTable;
+        private DataDirectory _vtableFixups;
+        private DataDirectory _exportAddressTableJumps;
+        private DataDirectory _managedNativeHeader;
+
         public Cor20Header(byte[] fileContents, uint address)
         {
             Offset offset = (int)address;
 
-            this.CB = BitConverter.ToUInt32(fileContents, offset.Shift(4));
-            this.MajorRuntimeVersion = BitConverter.ToUInt16(fileContents, offset.Shift(2));
-            this.MinorRuntimeVersion = BitConverter.ToUInt16(fileContents, offset.Shift(2));
-            this.MetaData = new DataDirectory(GetRange(fileContents, offset.Shift(8), 8));
-            this.Flags = (Cor20Flags)BitConverter.ToUInt32(fileContents, offset.Shift(4));
-            this.EntryPointToken = BitConverter.ToUInt32(fileContents, offset.Shift(4));
-            this.Resources = new DataDirectory(GetRange(fileContents, offset.Shift(8), 8));
-            this.StrongNameSigniture = new DataDirectory(GetRange(fileContents, offset.Shift(8), 8));
-            this.CodeManagerTable = new DataDirectory(GetRange(fileContents, offset.Shift(8), 8));
-            this.VTableFixups = new DataDirectory(GetRange(fileContents, offset.Shift(8), 8));
-            this.ExportAddressTableJumps = new DataDirectory(GetRange(fileContents, offset.Shift(8), 8));
-            this.ManagedNativeHeader = new DataDirectory(GetRange(fileContents, offset.Shift(8), 8));
+            _cb = BitConverter.ToUInt32(fileContents, offset.Shift(4));
+            _majorRuntimeVersion = BitConverter.ToUInt16(fileContents, offset.Shift(2));
+            _minorRuntimeVersion = BitConverter.ToUInt16(fileContents, offset.Shift(2));
+            _metadata = new DataDirectory(GetRange(fileContents, offset.Shift(8), 8));
+            _flags = (Cor20Flags)BitConverter.ToUInt32(fileContents, offset.Shift(4));
+            _entryPointToken = BitConverter.ToUInt32(fileContents, offset.Shift(4));
+            _resources = new DataDirectory(GetRange(fileContents, offset.Shift(8), 8));
+            _strongNameSigniture = new DataDirectory(GetRange(fileContents, offset.Shift(8), 8));
+            _codeManagerTable = new DataDirectory(GetRange(fileContents, offset.Shift(8), 8));
+            _vtableFixups = new DataDirectory(GetRange(fileContents, offset.Shift(8), 8));
+            _exportAddressTableJumps = new DataDirectory(GetRange(fileContents, offset.Shift(8), 8));
+            _managedNativeHeader = new DataDirectory(GetRange(fileContents, offset.Shift(8), 8));
         }
 
         private byte[] GetRange(byte[] fileContents, Offset start, int length)
@@ -38,38 +51,64 @@ namespace TheBoxSoftware.Reflection.Core.COFF
         /// <summary>
         /// Size of the header in bytes
         /// </summary>
-        public UInt64 CB { get; set; }
+        public ulong CB
+        {
+            get { return _cb; }
+            set { _cb = value; }
+        }
 
         /// <summary>
         /// Major portion of the minimum version of the runtime required
         /// </summary>
-        public UInt16 MajorRuntimeVersion { get; set; }
+        public ushort MajorRuntimeVersion
+        {
+            get { return _majorRuntimeVersion; }
+            set { _majorRuntimeVersion = value; }
+        }
 
         /// <summary>
         /// Minor portion of the minimum version of the runtime required
         /// </summary>
-        public UInt16 MinorRuntimeVersion { get; set; }
+        public ushort MinorRuntimeVersion
+        {
+            get { return _minorRuntimeVersion; }
+            set { _minorRuntimeVersion = value; }
+        }
 
         /// <summary>
         /// RVA and size of the meta data
         /// </summary>
-        public DataDirectory MetaData { get; set; }
+        public DataDirectory MetaData
+        {
+            get { return _metadata; }
+            set { _metadata = value; }
+        }
 
-        /// <summary>
-        /// </summary>
-        public Cor20Flags Flags { get; set; }
+        public Cor20Flags Flags
+        {
+            get { return _flags; }
+            set { _flags = value; }
+        }
 
         /// <summary>
         /// Metadata identifier of the entry poitn for the image file. Can be 0 for DLL
         /// images. This field identifies a method belonging to this module or a module
         /// containing the entry point method.
         /// </summary>
-        public UInt64 EntryPointToken { get; set; }
+        public ulong EntryPointToken
+        {
+            get { return _entryPointToken; }
+            set { _entryPointToken = value; }
+        }
 
         /// <summary>
         /// RVA and size of managed resources
         /// </summary>
-        public DataDirectory Resources { get; set; }
+        public DataDirectory Resources
+        {
+            get { return _resources; }
+            set { _resources = value; }
+        }
 
         /// <summary>
         /// RVA and size of the hash data fo rthis PE file, used by the loader for binding
@@ -77,8 +116,8 @@ namespace TheBoxSoftware.Reflection.Core.COFF
         /// </summary>
         public DataDirectory StrongNameSigniture
         {
-            get;
-            set;
+            get { return _strongNameSigniture; }
+            set { _strongNameSigniture = value; }
         }
 
         /// <summary>
@@ -86,8 +125,8 @@ namespace TheBoxSoftware.Reflection.Core.COFF
         /// </summary>
         public DataDirectory CodeManagerTable
         {
-            get;
-            set;
+            get { return _codeManagerTable; }
+            set { _codeManagerTable = value; }
         }
 
         /// <summary>
@@ -95,16 +134,28 @@ namespace TheBoxSoftware.Reflection.Core.COFF
         /// managed compilers, only the MC++ compiler and linker and the ILAsm compiler can
         /// produce this array.
         /// </summary>
-        public DataDirectory VTableFixups { get; set; }
+        public DataDirectory VTableFixups
+        {
+            get { return _vtableFixups; }
+            set { _vtableFixups = value; }
+        }
 
         /// <summary>
         /// RVA and size of an array of addresses of jump thunks
         /// </summary>
-        public DataDirectory ExportAddressTableJumps { get; set; }
+        public DataDirectory ExportAddressTableJumps
+        {
+            get { return _exportAddressTableJumps; }
+            set { _exportAddressTableJumps = value; }
+        }
 
         /// <summary>
         /// Reserved set to zero
         /// </summary>
-        public DataDirectory ManagedNativeHeader { get; set; }
+        public DataDirectory ManagedNativeHeader
+        {
+            get { return _managedNativeHeader; }
+            set { _managedNativeHeader = value; }
+        }
     }
 }
