94afec5bc016210ac9054c24c31e114dec9799c3
barry <atirez@bitbucket.org>
2016-11-09 08:05:57 +0000

    reformatted code to be more readable.

--- Source/TheBoxSoftware.DeveloperSuite.LiveDocumenter/Pages/NamespacePage.cs
+++ Source/TheBoxSoftware.DeveloperSuite.LiveDocumenter/Pages/NamespacePage.cs
@@ -1,111 +1,112 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows.Documents;
-using System.Xml;
+﻿
+namespace TheBoxSoftware.DeveloperSuite.LiveDocumenter.Pages
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows.Documents;
+    using TheBoxSoftware.Reflection;
+    using TheBoxSoftware.Reflection.Comments;
+    using TheBoxSoftware.DeveloperSuite.LiveDocumenter.Pages.Elements;
 
-namespace TheBoxSoftware.DeveloperSuite.LiveDocumenter.Pages {
-	using TheBoxSoftware.Reflection;
-	using TheBoxSoftware.Reflection.Comments;
-	using TheBoxSoftware.DeveloperSuite.LiveDocumenter.Pages.Elements;
+    /// <summary>
+    /// A Page to display information about a namespace in the LiveDocumentor
+    /// </summary>
+    public class NamespacePage : Page
+    {
+        private KeyValuePair<string, List<TypeDef>> item;
+        private XmlCodeCommentFile commentsXml;
 
-	/// <summary>
-	/// A Page to display information about a namespace in the LiveDocumentor
-	/// </summary>
-	public class NamespacePage : Page {
-		private KeyValuePair<string, List<TypeDef>> item;
-		private XmlCodeCommentFile commentsXml;
+        /// <summary>
+        /// Initialises a new instance of the NamespacePage class
+        /// </summary>
+        /// <param name="item">The namespace details as a list of methods</param>
+        /// <param name="commentsXml">The code comments file to get comments from</param>
+        public NamespacePage(KeyValuePair<string, List<TypeDef>> item, XmlCodeCommentFile commentsXml)
+        {
+            this.item = item;
+            this.commentsXml = commentsXml;
+        }
 
-		#region Constructors
-		/// <summary>
-		/// Initialises a new instance of the NamespacePage class
-		/// </summary>
-		/// <param name="item">The namespace details as a list of methods</param>
-		/// <param name="commentsXml">The code comments file to get comments from</param>
-		public NamespacePage(KeyValuePair<string, List<TypeDef>> item, XmlCodeCommentFile commentsXml) {
-			this.item = item;
-			this.commentsXml = commentsXml;
-		}
-		#endregion
+        /// <summary>
+        /// Generates the contents of the page
+        /// </summary>
+        public override void Generate()
+        {
+            if(!this.IsGenerated)
+            {
+                XmlCodeCommentFile xmlFile = commentsXml.GetReusableFile();
 
-		#region Methods
-		/// <summary>
-		/// Generates the contents of the page
-		/// </summary>
-		public override void Generate() {
-			if (!this.IsGenerated) {
-				XmlCodeCommentFile xmlFile = commentsXml.GetReusableFile();
+                this.Blocks.Add(new Header1(item.Key + " Namespace"));
 
-				this.Blocks.Add(new Header1(item.Key + " Namespace"));
+                // classes
+                IOrderedEnumerable<TypeDef> allClasses = from type in item.Value
+                                                         where !type.IsDelegate && !type.IsEnumeration && !type.IsInterface && !type.IsStructure &&
+                                                            !LiveDocumentorFile.Singleton.LiveDocument.IsMemberFiltered(type)
+                                                         orderby type.Name
+                                                         select type;
+                this.OutputTypes("Classes", allClasses, xmlFile);
 
-				// classes
-				IOrderedEnumerable<TypeDef> allClasses = from type in item.Value
-								 where !type.IsDelegate && !type.IsEnumeration && !type.IsInterface && !type.IsStructure &&
-									!LiveDocumentorFile.Singleton.LiveDocument.IsMemberFiltered(type)
-								 orderby type.Name
-								 select type;
-				this.OutputTypes("Classes", allClasses, xmlFile);
+                // structures
+                IOrderedEnumerable<TypeDef> allStructures = from type in item.Value
+                                                            where type.IsStructure &&
+                                                               !LiveDocumentorFile.Singleton.LiveDocument.IsMemberFiltered(type)
+                                                            orderby type.Name
+                                                            select type;
+                this.OutputTypes("Structures", allStructures, xmlFile);
 
-				// structures
-				IOrderedEnumerable<TypeDef> allStructures = from type in item.Value
-								 where type.IsStructure &&
-									!LiveDocumentorFile.Singleton.LiveDocument.IsMemberFiltered(type)
-								 orderby type.Name
-								 select type;
-				this.OutputTypes("Structures", allStructures, xmlFile);
+                // delegates
+                IOrderedEnumerable<TypeDef> allDelegates = from type in item.Value
+                                                           where type.IsDelegate &&
+                                                              !LiveDocumentorFile.Singleton.LiveDocument.IsMemberFiltered(type)
+                                                           orderby type.Name
+                                                           select type;
+                this.OutputTypes("Delegates", allDelegates, xmlFile);
 
-				// delegates
-				IOrderedEnumerable<TypeDef> allDelegates = from type in item.Value
-								 where type.IsDelegate &&
-									!LiveDocumentorFile.Singleton.LiveDocument.IsMemberFiltered(type)
-								 orderby type.Name
-								 select type;
-				this.OutputTypes("Delegates", allDelegates, xmlFile);
+                // enumerations
+                IOrderedEnumerable<TypeDef> allEnumerations = from type in item.Value
+                                                              where type.IsEnumeration &&
+                                                                 !LiveDocumentorFile.Singleton.LiveDocument.IsMemberFiltered(type)
+                                                              orderby type.Name
+                                                              select type;
+                this.OutputTypes("Enumerations", allEnumerations, xmlFile);
 
-				// enumerations
-				IOrderedEnumerable<TypeDef> allEnumerations = from type in item.Value
-								 where type.IsEnumeration &&
-									!LiveDocumentorFile.Singleton.LiveDocument.IsMemberFiltered(type)
-								 orderby type.Name
-								 select type;
-				this.OutputTypes("Enumerations", allEnumerations, xmlFile);
+                // interfaces
+                IOrderedEnumerable<TypeDef> allInterfaces = from type in item.Value
+                                                            where type.IsInterface &&
+                                                               !LiveDocumentorFile.Singleton.LiveDocument.IsMemberFiltered(type)
+                                                            orderby type.Name
+                                                            select type;
+                this.OutputTypes("Interfaces", allInterfaces, xmlFile);
 
-				// interfaces
-				IOrderedEnumerable<TypeDef> allInterfaces = from type in item.Value
-								 where type.IsInterface &&
-									!LiveDocumentorFile.Singleton.LiveDocument.IsMemberFiltered(type)
-								 orderby type.Name
-								 select type;
-				this.OutputTypes("Interfaces", allInterfaces, xmlFile);
+                this.IsGenerated = true;
+            }
+        }
 
-				this.IsGenerated = true;
-			}
-		}
+        /// <summary>
+        /// Adds a table of the <paramref name="types"/> to the document with a header <paramref name="name"/>.
+        /// </summary>
+        /// <param name="name">The header text.</param>
+        /// <param name="types">The types to add to the table.</param>
+        /// <param name="xmlFile">The XML file to read comments from.</param>
+        private void OutputTypes(string name, IOrderedEnumerable<TypeDef> types, XmlCodeCommentFile xmlFile)
+        {
+            if(types.Count() != 0)
+            {
+                SummaryTable classTable = new SummaryTable();
+                foreach(TypeDef currentType in types)
+                {
+                    CRefPath crefPath = new CRefPath(currentType);
 
-		/// <summary>
-		/// Adds a table of the <paramref name="types"/> to the document with a header <paramref name="name"/>.
-		/// </summary>
-		/// <param name="name">The header text.</param>
-		/// <param name="types">The types to add to the table.</param>
-		/// <param name="xmlFile">The XML file to read comments from.</param>
-		private void OutputTypes(string name, IOrderedEnumerable<TypeDef> types, XmlCodeCommentFile xmlFile) {
-			if (types.Count() != 0) {
-				SummaryTable classTable = new SummaryTable();
-				foreach (TypeDef currentType in types) {
-					CRefPath crefPath = new CRefPath(currentType);
-
-					// Find the description for the type
-					Block description = this.GetSummaryFor(xmlFile, currentType.Assembly, "/doc/members/member[@name='" + crefPath + "']/summary");
-					Hyperlink nameLink = new Hyperlink(new Run(currentType.GetDisplayName(false)));
-					nameLink.Tag = new EntryKey(currentType.GetGloballyUniqueId());
-					nameLink.Click += new System.Windows.RoutedEventHandler(LinkHelper.Resolve);
-					classTable.AddItem(nameLink, description, Model.ElementIconConstants.GetIconPathFor(currentType));
-				}
-				this.Blocks.Add(new Header2(name));
-				this.Blocks.Add(classTable);
-			}
-		}
-		#endregion
-	}
+                    // Find the description for the type
+                    Block description = this.GetSummaryFor(xmlFile, currentType.Assembly, "/doc/members/member[@name='" + crefPath + "']/summary");
+                    Hyperlink nameLink = new Hyperlink(new Run(currentType.GetDisplayName(false)));
+                    nameLink.Tag = new EntryKey(currentType.GetGloballyUniqueId());
+                    nameLink.Click += new System.Windows.RoutedEventHandler(LinkHelper.Resolve);
+                    classTable.AddItem(nameLink, description, Model.ElementIconConstants.GetIconPathFor(currentType));
+                }
+                this.Blocks.Add(new Header2(name));
+                this.Blocks.Add(classTable);
+            }
+        }
+    }
 }
--- Source/TheBoxSoftware.DeveloperSuite.LiveDocumenter/Pages/TypeFieldsPage.cs
+++ Source/TheBoxSoftware.DeveloperSuite.LiveDocumenter/Pages/TypeFieldsPage.cs
@@ -1,90 +1,99 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Xml;
-using System.Windows.Documents;
+﻿
+namespace TheBoxSoftware.DeveloperSuite.LiveDocumenter.Pages
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows.Documents;
+    using TheBoxSoftware.DeveloperSuite.LiveDocumenter.Pages.Elements;
+    using TheBoxSoftware.Reflection;
+    using TheBoxSoftware.Reflection.Comments;
 
-namespace TheBoxSoftware.DeveloperSuite.LiveDocumenter.Pages {
-	using TheBoxSoftware.DeveloperSuite.LiveDocumenter.Pages.Elements;
-	using TheBoxSoftware.Reflection;
-	using TheBoxSoftware.Reflection.Comments;
+    /// <summary>
+    /// A page that displays all of the fields that are available in a type
+    /// </summary>
+    public sealed class TypeFieldsPage : Page
+    {
+        private List<FieldDef> fields;
+        private XmlCodeCommentFile xmlComments;
 
-	/// <summary>
-	/// A page that displays all of the fields that are available in a type
-	/// </summary>
-	public sealed class TypeFieldsPage : Page {
-		private List<FieldDef> fields;
-		private XmlCodeCommentFile xmlComments;
+        /// <summary>
+        /// Initialises a new instance of the TypeFieldsPage class
+        /// </summary>
+        /// <param name="fields">The fields to manage</param>
+        /// <param name="xmlComments">The xml comments</param>
+        public TypeFieldsPage(List<FieldDef> fields, XmlCodeCommentFile xmlComments)
+        {
+            this.fields = fields;
+            this.xmlComments = xmlComments;
+        }
 
-		/// <summary>
-		/// Initialises a new instance of the TypeFieldsPage class
-		/// </summary>
-		/// <param name="fields">The fields to manage</param>
-		/// <param name="xmlComments">The xml comments</param>
-		public TypeFieldsPage(List<FieldDef> fields, XmlCodeCommentFile xmlComments) {
-			this.fields = fields;
-			this.xmlComments = xmlComments;
-		}
+        /// <summary>
+        /// Generates the pages contents
+        /// </summary>
+        public override void Generate()
+        {
+            if(!this.IsGenerated)
+            {
+                TypeRef definingType = null;
+                if(fields.Count > 0)
+                {
+                    definingType = fields[0].Type;
+                }
+                XmlCodeCommentFile comments = this.xmlComments.GetReusableFile();
 
-		/// <summary>
-		/// Generates the pages contents
-		/// </summary>
-		public override void Generate() {
-			if (!this.IsGenerated) {
-				TypeRef definingType = null;
-				if (fields.Count > 0) {
-					definingType = fields[0].Type;					
-				}
-				XmlCodeCommentFile comments = this.xmlComments.GetReusableFile();
+                if(!this.xmlComments.Exists())
+                {
+                    this.Blocks.Add(new NoXmlComments(definingType));
+                }
 
-				if (!this.xmlComments.Exists()) {
-					this.Blocks.Add(new NoXmlComments(definingType));
-				}
+                this.Blocks.Add(new Header1(definingType.GetDisplayName(false) + " Fields"));
 
-				this.Blocks.Add(new Header1(definingType.GetDisplayName(false) + " Fields"));
+                if(this.fields != null && this.fields.Count > 0)
+                {
+                    SummaryTable displayedFields = new SummaryTable();
 
-				if (this.fields != null && this.fields.Count > 0) {
-					SummaryTable displayedFields = new SummaryTable();
+                    var sortedFields = from field in this.fields
+                                       orderby field.Name
+                                       where !LiveDocumentorFile.Singleton.LiveDocument.IsMemberFiltered(field)
+                                       select field;
+                    foreach(FieldDef currentField in sortedFields)
+                    {
+                        CRefPath crefPath = new CRefPath(currentField);
+                        System.Windows.Documents.Hyperlink link = new System.Windows.Documents.Hyperlink();
+                        link.Inlines.Add(new System.Windows.Documents.Run(currentField.Name));
+                        link.Tag = new EntryKey(currentField.GetGloballyUniqueId());
+                        link.Click += new System.Windows.RoutedEventHandler(LinkHelper.Resolve);
 
-					var sortedFields = from field in this.fields
-									   orderby field.Name
-									   where !LiveDocumentorFile.Singleton.LiveDocument.IsMemberFiltered(field)
-									   select field;
-					foreach (FieldDef currentField in sortedFields) {
-						CRefPath crefPath = new CRefPath(currentField);
-						System.Windows.Documents.Hyperlink link = new System.Windows.Documents.Hyperlink();
-						link.Inlines.Add(new System.Windows.Documents.Run(currentField.Name));
-						link.Tag = new EntryKey(currentField.GetGloballyUniqueId());
-						link.Click += new System.Windows.RoutedEventHandler(LinkHelper.Resolve);
+                        // First we check if there is a summary for the field, then if not we check for a
+                        // definition of value and use that if it is defined.
+                        Block summary = null;
+                        XmlCodeComment comment = comments.ReadComment(
+                            "/doc/members/member[@name='" + crefPath.ToString() + "']/summary"
+                            );
+                        List<Block> parsedBlocks = Elements.Parser.Parse(currentField.Assembly, comment);
+                        if(parsedBlocks != null && parsedBlocks.Count > 0)
+                        {
+                            summary = parsedBlocks[0];
+                        }
+                        else
+                        {
+                            XmlCodeComment value = comments.ReadComment(
+                                "/doc/members/member[@name='" + crefPath.ToString() + "']/value"
+                                );
+                            parsedBlocks = Elements.Parser.Parse(currentField.Assembly, value);
+                            if(parsedBlocks != null && parsedBlocks.Count > 0)
+                            {
+                                summary = parsedBlocks[0];
+                            }
+                        }
 
-						// First we check if there is a summary for the field, then if not we check for a
-						// definition of value and use that if it is defined.
-						Block summary = null;
-						XmlCodeComment comment = comments.ReadComment(
-							"/doc/members/member[@name='" + crefPath.ToString() + "']/summary"
-							);
-						List<Block> parsedBlocks = Elements.Parser.Parse(currentField.Assembly, comment);
-						if (parsedBlocks != null && parsedBlocks.Count > 0) {
-							summary = parsedBlocks[0];
-						}
-						else {
-							XmlCodeComment value = comments.ReadComment(
-								"/doc/members/member[@name='" + crefPath.ToString() + "']/value"
-								);
-							parsedBlocks = Elements.Parser.Parse(currentField.Assembly, value);
-							if (parsedBlocks != null && parsedBlocks.Count > 0) {
-								summary = parsedBlocks[0];
-							}
-						}
+                        displayedFields.AddItem(link, summary, Model.ElementIconConstants.GetIconPathFor(currentField));
+                    }
+                    this.Blocks.Add(displayedFields);
+                }
 
-						displayedFields.AddItem(link, summary, Model.ElementIconConstants.GetIconPathFor(currentField));
-					}
-					this.Blocks.Add(displayedFields);
-				}
-
-				this.IsGenerated = true;
-			}
-		}
-	}
+                this.IsGenerated = true;
+            }
+        }
+    }
 }
