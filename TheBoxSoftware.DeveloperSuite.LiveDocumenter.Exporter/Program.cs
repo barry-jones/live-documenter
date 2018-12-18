352acc5733f88c427dbd42abb3d8e41fdeee24db
Barry Jones <barry_jones78@hotmail.com>
2018-12-18 13:30:34 +0000

    migrated the exporter console application

---
+++ TheBoxSoftware.DeveloperSuite.LiveDocumenter.Exporter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheBoxSoftware.DeveloperSuite.LiveDocumenter.Exporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
