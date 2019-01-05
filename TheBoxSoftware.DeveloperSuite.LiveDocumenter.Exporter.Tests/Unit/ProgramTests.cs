c6ab1570a380049ab943ec10e0ab96e036ca34d9
Barry Jones <github@barryjones.me.uk>
2019-01-05 12:08:24 +0000

    Continued refactoring work. Removed another Console reference from program, fixed some issues with visibility and testing with Moq.

---
+++ TheBoxSoftware.DeveloperSuite.LiveDocumenter.Exporter.Tests/Unit/ProgramTests.cs
@@ -0,0 +1,22 @@
+﻿
+namespace TheBoxSoftware.DeveloperSuite.LiveDocumenter.Exporter.Tests.Unit
+{
+    using TheBoxSoftware.Exporter;
+    using NUnit.Framework;
+    using Moq;
+
+    [TestFixture]
+    public class ProgramTests
+    {
+        [Test]
+        public void Program_Create()
+        {
+            Mock<IUserInterface> ui = new Mock<IUserInterface>();
+            Mock<ILog> log = new Mock<ILog>();
+            Mock<IFileSystem> filesystem = new Mock<IFileSystem>();
+            string[] arguments = new string[] { };
+            
+            Program p = new Program(arguments, filesystem.Object, ui.Object, log.Object);
+        }
+    }
+}
