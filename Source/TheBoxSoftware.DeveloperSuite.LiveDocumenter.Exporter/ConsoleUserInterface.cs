2fe4a02e8c43a2ec6521d83b389e0b08b254ef5e
Barry Jones <github@barryjones.me.uk>
2019-01-05 11:56:22 +0000

    Seperated log and console system code to interfaces and implementations to enable testing of Program and Exporter classes. Refactoring work.

---
+++ Source/TheBoxSoftware.DeveloperSuite.LiveDocumenter.Exporter/ConsoleUserInterface.cs
@@ -0,0 +1,25 @@
+﻿
+namespace TheBoxSoftware.Exporter
+{
+    using System;
+
+    internal class ConsoleUserInterface : IUserInterface
+    {
+        public ConsoleColor ForegroundColor { set => Console.ForegroundColor = value; }
+
+        public void ResetColor()
+        {
+            Console.ResetColor();
+        }
+
+        public void Write(string content)
+        {
+            Console.Write(content);
+        }
+
+        public void WriteLine(string content)
+        {
+            Console.WriteLine(content);
+        }
+    }
+}
---
+++ Source/TheBoxSoftware.DeveloperSuite.LiveDocumenter.Exporter/IUserInterface.cs
@@ -0,0 +1,18 @@
+﻿
+namespace TheBoxSoftware.Exporter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    internal interface IUserInterface
+    {
+        void Write(string content);
+
+        void WriteLine(string content);
+
+        void ResetColor();
+
+        ConsoleColor ForegroundColor { set; }
+    }
+}
