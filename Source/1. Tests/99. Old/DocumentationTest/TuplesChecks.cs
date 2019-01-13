337bc4442dc636d42005cea470106a755af0d641
Barry Jones <barry_jones78@hotmail.com>
2019-01-13 15:27:43 +0000

    added TuplesChecks class to documentation test library

---
+++ Source/1. Tests/99. Old/DocumentationTest/TuplesChecks.cs
@@ -0,0 +1,16 @@
+﻿
+namespace DocumentationTest
+{
+    public class TuplesChecks
+    {
+        public (string, string) UnamedTuplesReturned()
+        {
+            return (string.Empty, string.Empty);
+        }
+
+        public (int first, int second) NamedTuplesReturned()
+        {
+            return (1, 2);
+        }
+    }
+}
